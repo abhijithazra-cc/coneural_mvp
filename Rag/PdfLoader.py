5740cac430b85be5db6556df63b4e2e6a15f0a6c
unknown <rahul.kumar@collectcent.com>
2025-11-05 11:46:54 +0530

    second commit

---
+++ Rag/PdfLoader.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import PyMuPDFLoader
+from abstractions.Idocloader import Idocloader
+
+class PdfLoader(Idocloader):
+     def __init__(self):
+          self.documents=""
+     def load_document(self,file_path=""):
+          loader=PyMuPDFLoader(file_path)
+          self.documents=loader.load()
+          return self
+     def get_document(self):
+          return self.documents
+# file_path = "./example_data/layout-parser-paper.pdf"
+# loader = PyMuPDFLoader(file_path)
