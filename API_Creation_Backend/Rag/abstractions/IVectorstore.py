db41dbe507d96f3eec315b3f006b5f64acce20c9
rahulcollectcent <collectcent@DESKTOP-Q5GT70V.localdomain>
2025-12-15 06:09:56 +0000

    changing db schema v1

--- API_Creation_Backend/Rag/abstractions/IVectorstore.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-class IVectorstore(ABC):
-      @abstractmethod
-      def set_vector_store(docs,embeddings):
-          pass
-      @abstractmethod
-      def get_vector_store():
-          pass
-      
--- API_Creation_Backend/Rag/abstractions/Idocloader.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-class Idocloader(ABC):
-      @abstractmethod
-      def load_document(file_path=""):
-          pass
-      @abstractmethod
-      def get_document():
-           pass
